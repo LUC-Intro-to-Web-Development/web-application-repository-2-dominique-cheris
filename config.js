a32d52c350b468286eba24eb8161899230946755
dcheris <97857929+dcheris@users.noreply.github.com>
2022-11-21 22:22:32 -0600

    server foundation.

---
+++ config.js
@@ -0,0 +1,3 @@
+var sqlite3 = require('sqlite3').verbose();
+
+let db = new sqlite3.Database('')
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
+// adding port
+const port = 8080;
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
