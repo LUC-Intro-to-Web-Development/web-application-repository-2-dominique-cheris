99df937db24194e1e9f3a7d79485c030b4421637
dcheris <97857929+dcheris@users.noreply.github.com>
2022-11-22 00:24:51 -0600

    correcting routing

--- app.js
+++ app.js
@@ -19,16 +19,28 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// ADDING A SESSION FUNCTION
+/*
+app.use(
+  session({
+    secret: 'arbitrary-string',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {secure: true}
+  })
+);
+*/
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use((req, res, next) => {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use((err, req, res, next) => {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
--- routes/index.js
+++ routes/index.js
@@ -1,56 +1,9 @@
-const { application } = require('express');
 var express = require('express');
 var router = express.Router();
 
-// ADDING PORT
-const port = 8080;
-
-// CREATING FUNCTION FOR APP
-app.use('/home', (req, res, next) => {
-  console.log('A new request recieved at');
-  next();
-});
-
-app.get('/home', (req, res,) => {
-  res.send('Home Page');
-});
-/* 
-GET home page. 
+/* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
-*/
-
-  // ADDITIONAL ROUTE PATHS
-  /*
-  app.get('/home', (req, res) => {
-    res.send('Home Page');
-  });
-  app.get('/about', (req, res) => {
-    res.send('About');
-  });
-  */
-
-  // ROUTING PARAMETER TEMPLATE. 
-  /*
-  app.get('/books/:bookId', (req, res) => {
-    res.send(req.params);
-  });
-
-  // FOR INVALID ROUTES
-app.get('*', (req, res) => {
-  res.send('404! This is an invalid URL.');
 });
-  */
-
-  // ADDING ROUTE METHOD TEMPLATE
-//app.METHOD(PATH, HANDLER);
-
-
 
 module.exports = router;
-
-  // ADDING PORT LISTENER 
-  app.listen(8080, () => console.log('Example app listening on port 8080'));
-
-
-
