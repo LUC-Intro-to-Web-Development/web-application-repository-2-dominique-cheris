cf37d33ef175f8ab47cdeac1c78b40322cb2722b
dcheris <97857929+dcheris@users.noreply.github.com>
2022-11-24 23:32:30 -0600

    completing basic routing.

--- routes/index.js
+++ routes/index.js
@@ -1,35 +1,6 @@
 var express = require('express');
 var router = express.Router();
 
-// THIS FILE CHANGES WHAT IS ON THE WEBPAGE
-
-
-// ROUTE TO CREATE 
-router.post('/create', function (req, res) {
-	console.log("Sent as a get request");
-	res.render('index')
-})
-/*
-// ROUTE TO UPDATE 
-router.post('/update', function (req, res) {
-	console.log("Sent as a get request");
-
-	res.render('index')
-})
-
-// ROUTE TO LIST 
-router.post('/list', function (req, res) {
-	console.log("Sent as a get request");
-
-	res.render('index', { title: "HELLO WORLD!"})
-})
-
-// ROUTE TO DELETE 
-router.post('/delete', function (req, res) {
-	console.log("Sent as a get request");
-
-	res.render('index', { title: "HELLO WORLD!"})
-})
 
 router.get('/users/:id', function (req, res) {
 	//Getting id parameter
@@ -54,5 +25,5 @@ router.get('/testing', function (req, res) {
 	console.log(firstName + " " + lastName + " " + id );
 	res.render( 'index', {title : "Contact Page"})
  })
-*/
+
 module.exports = router;
