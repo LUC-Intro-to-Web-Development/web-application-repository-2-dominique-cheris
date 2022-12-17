d2721e372e746ea4fcb486a6f8cd471e1e03b29f
dcheris <97857929+dcheris@users.noreply.github.com>
2022-12-16 19:53:32 -0600

    fixing procfile.

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const dbOperations = require('./database.js');
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 const bcrypt = require('bcrypt');
 const mongoose = require('mongoose');
 
